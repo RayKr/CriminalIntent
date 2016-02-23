2147085e37915f20f888d29dc21167a66969592a
raykr <ray_kr@163.com>
2016-02-23 23:24:06 +0800

    UI fragment

---
+++ src/com/bignerdranch/android/cirminalintent/Crime.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.cirminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Ray on 2016/2/23.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
