6ad86531748190f82a2ade3507f62fa61e511f3f
raykr <ray_kr@163.com>
2016-02-24 10:39:53 +0800

    添加layout-land

--- src/com/bignerdranch/android/cirminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/cirminalintent/CrimeFragment.java
@@ -4,10 +4,15 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import com.bignerdranch.android.cirminalintent.model.Crime;
 
 /**
  * Created by Ray on 2016/2/23.
@@ -16,6 +21,8 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSovledCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,7 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
+        // 显示标题默认提示
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
@@ -42,6 +50,20 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        // 显示日期
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(DateFormat.format("yyyy-mm-dd hh:mm:ss", mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        // Solved?
+        mSovledCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSovledCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
 
--- src/com/bignerdranch/android/cirminalintent/model/Crime.java
+++ src/com/bignerdranch/android/cirminalintent/model/Crime.java
@@ -1,5 +1,6 @@
-package com.bignerdranch.android.cirminalintent;
+package com.bignerdranch.android.cirminalintent.model;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,25 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 
     public UUID getId() {
