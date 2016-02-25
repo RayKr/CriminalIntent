6e3fc7f7d1000f9c5efa5b86c98358a8142e3311
raykr <ray_kr@163.com>
2016-02-25 17:27:36 +0800

    增加列表显示，使用到了ArrayAdapter、内部类等

--- src/com/bignerdranch/android/cirminalintent/CrimeListFragment.java
+++ src/com/bignerdranch/android/cirminalintent/CrimeListFragment.java
@@ -1,9 +1,19 @@
 package com.bignerdranch.android.cirminalintent;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 import com.bignerdranch.android.cirminalintent.model.Crime;
 import com.bignerdranch.android.cirminalintent.model.CrimeLab;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -11,6 +21,9 @@ import java.util.ArrayList;
  * Created by Ray on 2016/2/24.
  */
 public class CrimeListFragment extends ListFragment {
+
+    private static final String TAG = "CrimeListFragment";
+
     private ArrayList<Crime> mCrimes;
 
     @Override
@@ -19,5 +32,41 @@ public class CrimeListFragment extends ListFragment {
         getActivity().setTitle(R.string.crime_title);
 
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        CrimeAdapter adapter = new CrimeAdapter(mCrimes);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = ((CrimeAdapter) getListAdapter()).getItem(position);
+        Log.d(TAG, c.getTitle() + " was clicked.");
+    }
+
+    private class CrimeAdapter extends ArrayAdapter<Crime> {
+
+        public CrimeAdapter(ArrayList<Crime> crimes) {
+            super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+            }
+
+            Crime c = getItem(position);
+
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+
+            TextView dateTextView = (TextView) convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(DateFormat.format("yyyy-mm-dd hh:mm:ss", c.getDate()));
+
+            CheckBox solvedCheckBox = (CheckBox) convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+
+            return convertView;
+        }
     }
 }
